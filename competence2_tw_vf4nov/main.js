d7a446303abced136b280f86ab2318f77cec548a
CarTig <carolle.t.n@gmail.com>
2022-11-15 16:01:11 +0100

    ajouts des cards avec vuejs et axios

--- competence2_tw_vf4nov/main.js
+++ competence2_tw_vf4nov/main.js
@@ -10,3 +10,21 @@ createApp({
   }
 
 }).mount('#app')
+
+
+
+createApp({
+    data () {
+        return {
+            items: []
+        }
+    },
+
+    mounted () {
+        axios
+        .get('products.json')
+        .then(response => (this.items = response.data.items))
+    }
+
+
+}).mount('#app1')
